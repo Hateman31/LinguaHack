fb7750c2de93b72e15e4def570b3fc664b7ad0d1
Gerutro <gerutrogame@gmail.com>
2024-04-01 14:24:32 +0300

    start in recognition branch. Problems:  long.

---
+++ recognition.py
@@ -0,0 +1,20 @@
+import assemblyai as __aai
+import config as __cfg
+
+
+__aai.settings.api_key = __cfg.speech_recognition_token
+
+__config = __aai.TranscriptionConfig(
+    language_detection=False,
+    language_code="ru"
+)
+
+__transcriber = __aai.Transcriber(config=__config)
+
+
+def recognize(audio_file):
+    """Recognition file.
+
+    audio_file  :file to recognize"""
+    result = __transcriber.transcribe(audio_file).text
+    return result
---
+++ tests/test_recognition.py
@@ -0,0 +1,4 @@
+import recognition
+
+if __name__ == "__main__":
+    print(recognition.recognize("output.wav"))
