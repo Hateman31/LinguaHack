88e51039b3a227487b758855c425695223cad530
hateman31 <metilogrunge31@gmail.com>
2024-04-03 19:31:28 +0300

    created setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import psycopg2
+import config
+from pathlib import Path
+
+create_schema_sql = open('./schema.sql', encoding='utf-8').read() 
+
+def create_schema():
+    with psycopg2.connect(config.CONN_STR) as db:
+        with db.cursor() as cursor:
+            cursor.execute(create_schema_sql)
+            print('Schema created!')
+
+def create_folders():
+    audio_folder = Path.cwd() / 'audio'
+    if not audio_folder.exists():
+        audio_folder.mkdir()
+    print('Folder for audio was created!')
+
+def main():
+    create_folders()
+    create_schema()
+
+if __name__ == '__main__':
+    main()
+
