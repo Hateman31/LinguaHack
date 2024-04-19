f11458909a2f876500e6c02489f84db5a70178c7
trustill <gonya.kzn@gmail.com>
2024-04-19 20:32:56 +0300

    Теперь можно сохранять через список url-ов

--- bot.py
+++ bot.py
@@ -6,10 +6,12 @@ def send_video(channel_id, video_id):
     try:
         video = open(f'vid_{video_id}.mp4', 'rb')
         subtitles = open(f'sub_{video_id}.en.vtt', 'rb')
+
         msg_video = bot.send_video(channel_id, video)
         msg_sub = bot.send_document(channel_id, subtitles)
+
         file_id = [msg_video.video.file_id, msg_sub.document.file_id]
         return file_id
     except:
-        print('video not found')
+        print('file not found')
 
--- download_video.py
+++ download_video.py
@@ -1,54 +1,53 @@
 import yt_dlp
-import sqlite3 as db
 import bot
 import psycopg2
 from conn_preset import conn_str
 
 video_id = '%(id)s'
-
 class VideoTransfuse:
-    def __init__(self, url_video, channel_id):
+    # В url_video передавать список ↓↓↓
+    def __init__(self, channel_id, url_video):
         self.url_video = url_video
         self.channel_id = channel_id
-
+    # Сохраняем видео ↓↓↓
     def save_youtube_video(self):
         try:
-            ydl_opts = {
-                'format': 'mp4',
-                'outtmpl': f'vid_{video_id}.mp4'
-            }
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download(self.url_video)
+            for url in self.url_video:
+                ydl_opts = {
+                    'format': 'mp4',
+                    'outtmpl': f'vid_{video_id}.mp4'
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download(url)
         except:
             print('invalid link')
-
+    # А здесь сохраняем субититры) ↓↓↓
     def save_youtube_subtitles(self):
         try:
-            ydl_opts_sub = {
-                'writeautomaticsub': True,
-                'subtitlesformat': 'srt',
-                "skip_download": True,
-                'outtmpl': f'sub_{video_id}'
-            }
-            with yt_dlp.YoutubeDL(ydl_opts_sub) as ydl:
-                ydl.download(self.url_video)
+            for url in self.url_video:
+                ydl_opts_sub = {
+                    'writeautomaticsub': True,
+                    'subtitlesformat': 'srt',
+                    "skip_download": True,
+                    'outtmpl': f'sub_{video_id}'
+                }
+                with yt_dlp.YoutubeDL(ydl_opts_sub) as ydl:
+                    ydl.download(url)
         except:
             print('invalid link')
 
 
     def save_in_db(self):
-        video_id_str = self.url_video.split('=')[1]
-        file_id = bot.send_video(self.channel_id, video_id_str)
+        try:
+            for url in self.url_video:
+                video_id_str = url.split('=')[1] # Берем id видео для удобного сохранения
+                file_id = bot.send_video(self.channel_id, video_id_str)
 
-        with psycopg2.connect(conn_str) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('insert into quizzes (video_file_id, sub_file_id) values (%s, %s)',
-                               [file_id[0], file_id[1]])
-                conn.commit()
-                print('succesful')
-
-
-vid = VideoTransfuse('https://www.youtube.com/watch?v=J3mSVfMgZfA', '-1002105419957')
-# vid.save_youtube_video()
-# vid.save_youtube_subtitles()
-vid.save_in_db()
+                with psycopg2.connect(conn_str) as conn:
+                    with conn.cursor() as cursor:
+                        cursor.execute('insert into quizzes (video_file_id, sub_file_id) values (%s, %s)',
+                                       [file_id[0], file_id[1]])
+                        conn.commit()
+                        print('succesful video -', self.url_video.index(url))
+        except TypeError:
+            print('files missing')
