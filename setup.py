a7d221a254b2860c7601eccaa8fa9ad0b2f35057
serge <sergejgrigorev29@gmail.com>
2024-04-20 20:10:38 +0800

    заполнил БД тестовыми данными

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import psycopg2
 import config as __cfg
 from pathlib import Path
 
-create_schema_sql = open('./schema.sql', encoding='utf-8').read() 
+create_schema_sql = open('sql/schema.sql', encoding='utf-8').read()
+fill_tables_sql = open('sql/insert_tables.sql', encoding='utf-8').read()
 
 
 def create_schema():
-    with psycopg2.connect(__cfg.config_sql) as db:
+    with psycopg2.connect(__cfg.conn_str) as db:
         with db.cursor() as cursor:
             cursor.execute(create_schema_sql)
             print('Schema created!')
@@ -19,9 +20,18 @@ def create_folders():
     print('Folder for audio was created!')
 
 
+def fill_the_tables():
+    with psycopg2.connect(__cfg.conn_str) as conn:
+        print('Record is started')
+        with conn.cursor() as cursor:
+            cursor.execute(fill_tables_sql)
+            print('Record if finished')
+
+
 def main():
     create_folders()
     create_schema()
+    fill_the_tables()
 
 
 if __name__ == '__main__':
---
+++ sql/insert_tables.sql
@@ -0,0 +1,27 @@
+-- Внесени данных в таблицу квизы
+INSERT INTO public.quizzes (quiz_id, title, description) VALUES (1, 'Урок_1', 'For beginner');
+INSERT INTO public.quizzes (quiz_id, title, description) VALUES (2, 'Урок_2', 'For advanced');
+INSERT INTO public.quizzes (quiz_id, title, description) VALUES (3, 'Урок_3', 'For genius');
+
+-- Внесени данных в таблицу вопросы
+INSERT INTO public.questions (quiz_id, question_text) VALUES (1, 'My mother (to have) a bad headache.');
+INSERT INTO public.questions (quiz_id, question_text) VALUES (1, 'my friend (to study) two foreign languages?');
+INSERT INTO public.questions (quiz_id, question_text) VALUES (2, 'The teacher (to give out) us English magazines at every lesson.');
+INSERT INTO public.questions (quiz_id, question_text) VALUES (2, 'Every morning, she (to hurry) to the University.');
+
+-- Внесени данных в таблицу варианты ответов
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (1, 'has', True);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (1, 'am', False);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (1, 'has got', False);
+
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (2, 'Do my friend studys ...', False);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (2, 'Do study...', False);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (2, 'Does my friend study...', True);
+
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (3, 'gives out', True);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (3, 'give out', False);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (3, 'will give out', False);
+
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (4, 'hurry', False);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (4, 'hurries', True);
+INSERT INTO public.options (question_id, option_text, is_correct) VALUES (4, 'will hurry', False);
