700b426d082f3782f078a99760ce69309436701d
metilogrunge31@gmail.com <metilogrunge31@gmail.com>
2024-04-22 22:43:52 +0300

    del unnecessery comments

--- sql_handler.py
+++ sql_handler.py
@@ -4,31 +4,27 @@ import psycopg2
 
 def check_quiz_id(conn_str, sql, user_id):
     with psycopg2.connect(conn_str) as conn:
-        print('Connection is created')
         with conn.cursor() as cursor:
             cursor.execute(sql, [user_id])
             all_records = cursor.fetchone()
-            print('Connection was close')
     return all_records
 
 
 def get_info(conn_str, sql, extension_str):
     with psycopg2.connect(conn_str) as conn:
-        print('Connection is created')
         with conn.cursor() as cursor:
             cursor.execute(sql, [extension_str])
             all_records = cursor.fetchall()
-            print('Connection was close')
     return all_records
 
 
 def get_available_quest(conn_str, sql, user_id, quiz_id):
     with psycopg2.connect(conn_str) as conn:
-        print('Connection is created')
+
         with conn.cursor() as cursor:
             cursor.execute(sql, [user_id, quiz_id])
             all_records = cursor.fetchall()
-            print('Connection was close')
+
     return all_records
 
 
@@ -42,12 +38,9 @@ def editing_info(conn_str, sql, *extension_str):
 def check_answer(conn_str, user_answer, quiz_id):
     sql = 'select 1 from speech_test where quiz_id=%s and answer=%s'
     with psycopg2.connect(conn_str) as conn:
-        print('Connection is created')
+
         with conn.cursor() as cursor:
             cursor.execute(sql, [quiz_id, user_answer])
-            # all_records = cursor.fetchall()
-            # print('Connection was close')
-            # return all_records
             if cursor.fetchall():
                 return True
     return False
@@ -55,11 +48,12 @@ def check_answer(conn_str, user_answer, quiz_id):
 def get_speech_test(conn_str, quiz_id):
     sql = 'select question_text from speech_test where quiz_id=%s'
     with psycopg2.connect(conn_str) as conn:
-        print('Connection is created')
+
         with conn.cursor() as cursor:
             cursor.execute(sql, [quiz_id])
             result = cursor.fetchone()
-            print('Connection was close')
+            print(quiz_id)
+
             return result[0]
 
 if __name__ == '__main__':
